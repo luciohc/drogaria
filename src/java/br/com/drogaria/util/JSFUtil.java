b940399b8037fd6717abd93fa009e5461b046555
luciohc <luciohc@gmail.com>
2019-01-06 19:34:35 -0200

    Atualização do JSFUtil.java

--- src/java/br/com/drogaria/util/JSFUtil.java
+++ src/java/br/com/drogaria/util/JSFUtil.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Autor: Lúcio Henrique da Costa
  */
 package br.com.drogaria.util;
 
